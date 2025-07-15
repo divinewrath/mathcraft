86bd58d9ffe5646ce9a01d6a8c76088ddbe70199
Łukasz Wojciechowski <divine.wrath1@gmail.com>
2025-07-15 11:41:32 +0200

    Optimize service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,9 +1,13 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
+
 import { build, files, prerendered, version } from '$service-worker';
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
 import { registerRoute, NavigationRoute } from 'workbox-routing';
-import { CacheFirst, NetworkFirst } from 'workbox-strategies';
+import { CacheFirst, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 const precache_list = [...build, ...files, ...prerendered].map((s) => ({
     url: s,
@@ -12,25 +16,48 @@ const precache_list = [...build, ...files, ...prerendered].map((s) => ({
 precacheAndRoute(precache_list);
 cleanupOutdatedCaches();
 
-const pageStrategy = new CacheFirst({
-	cacheName: 'images-cache',
+const navigationHandler = new NetworkFirst({
+	cacheName: 'pages-cache',
+	networkTimeoutSeconds: 3,
 	plugins: [
-		new CacheableResponsePlugin({
-			statuses: [0, 200],
-		}),
+		new CacheableResponsePlugin({ statuses: [0, 200] }),
+		new ExpirationPlugin({ maxEntries: 50 }),
 	],
 });
 
+const navigationRoute = new NavigationRoute(navigationHandler);
+registerRoute(navigationRoute);
+
 registerRoute(
-	({ request }) => request.mode === 'navigate',
-	pageStrategy
+	({ request }) => request.destination === 'image',
+	new CacheFirst({
+		cacheName: 'images-cache',
+		plugins: [
+			new CacheableResponsePlugin({ statuses: [0, 200] }),
+			new ExpirationPlugin({
+				maxEntries: 100,
+				maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+			}),
+		],
+	})
 );
 
-const navigationHandler = new NetworkFirst({
-		cacheName: 'navigations-cache',
-	networkTimeoutSeconds: 3,
+registerRoute(
+	({ request }) => request.destination === 'font',
+	new CacheFirst({
+		cacheName: 'fonts-cache',
+		plugins: [
+			new CacheableResponsePlugin({ statuses: [0, 200] }),
+			new ExpirationPlugin({
+				maxEntries: 30,
+				maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+			}),
+		],
+	})
+);
+
+self.addEventListener('message', (event) => {
+	if (event.data && event.data.type === 'SKIP_WAITING') {
+		self.skipWaiting();
+	}
 });
-
-const navigationRoute = new NavigationRoute(navigationHandler, {});
-
-registerRoute(navigationRoute);
